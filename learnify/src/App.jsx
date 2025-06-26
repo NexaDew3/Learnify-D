a37504873642f9ffbcc79dc049713b038ae87607
AkshayReddy60 <158162320+AkshayReddy60@users.noreply.github.com>
2025-06-26 12:18:26 +0530

    chore: Initial Project Setup with Docs & Tracker (#1)
    * chore: Initial Project Setup with Docs & Tracker
    
    * chore: Initial Project Setup with Docs & Tracker

---
+++ learnify/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className='text-red-500 text-3xl p-10'>
+      <h1>Hello, NexaDew</h1>
+    </div>
+  );
+}
+
+export default App;
