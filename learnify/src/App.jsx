b664ccb57fbc14c5ed098c762a8747f704852c61
dhanushrenukuntla <dhanushrenukuntla7@gmail.com>
2025-06-27 15:40:53 +0530

    feat: [Integration] Add Login page using atomic (#6)
    Co-authored-by: Dhanush Renukuntla <dhanushrenukuntla@gmail.com>

--- learnify/src/App.jsx
+++ learnify/src/App.jsx
@@ -1,9 +1,10 @@
 import React from "react";
+import Login from "./components/Login/Login";
 
 function App() {
   return (
-    <div className="text-red-500 text-3xl p-10">
-      <h1>Hello, NexaDew</h1>
+    <div>
+      <Login />
     </div>
   );
 }
---
+++ learnify/src/components/Login/README.md
@@ -0,0 +1,12 @@
+# Login
+
+React component for **Login**.
+
+## Usage
+
+```jsx
+import Login from './Login';
+
+<Login />
+```
+[Login Component](https://docs.google.com/document/d/1b5QqNnsXqtyso-reYAq28oGCObBGZQOVt8MjL6r3HVU/edit?usp=sharing)
